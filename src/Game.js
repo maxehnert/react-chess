6cef9a83c01fd1d3b1b97f7e7fb4a207d60200e0
Max Ehnert <max.ehnert@gmail.com>
2016-01-05 22:08:58 -0600

    Added click handler to move pieces.

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,4 @@
-let knightPosition = [0, 0];
+let knightPosition = [1, 7];
 let observer = null;
 function emitChange() {
   observer(knightPosition);
@@ -6,7 +6,7 @@ function emitChange() {
 
 export function observe(o) {
   if (observer) {
-    thow new Error('Multiple observers not implemented');
+    throw new Error('Multiple observers not implemented');
   }
 
   observer = o;
@@ -16,4 +16,13 @@ export function observe(o) {
 export function moveKnight(toX, toY) {
   knightPosition = [toX, toY];
   emitChange();
+}
+
+export function canMoveKnight(toX, toY) {
+  const [x, y] = knightPosition
+  const dx = toX - x;
+  const dy = toY - y;
+
+  return (Math.abs(dx) === 2 && Math.abs(dy) === 1) ||
+         (Math.abs(dx) === 1 && Math.abs(dy) === 2);
 }
