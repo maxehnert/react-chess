57cb9bef2789971d59c0f77adb925a06695fd574
Max Ehnert <max.ehnert@gmail.com>
2016-02-11 23:15:51 -0600

    Added redux and began building out

---
+++ src/Reducers/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+
+const reducers = combineReducers({
+  pieces,
+  position
+});
+export default rootReducer;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import Board from './Board';
 import { observe } from './Game';
 
+
+import { createStore } from 'redux';
+import rootReducer from './Reducers/index';
+
 const rootEl = document.getElementById('root');
 
 observe(knightPosition =>
@@ -11,3 +15,10 @@ observe(knightPosition =>
     rootEl
   )
 );
+
+ReactDOM.render(
+  <Provider store={createStore(rootReducer)}>
+    <Board />
+  </Provider>,
+  rootEl
+)
